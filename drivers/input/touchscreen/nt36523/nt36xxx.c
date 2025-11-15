cf95b84adbab9e78256da39ec5de5bed1cf3e5f7
ymdzq <1422091+ymdzq@users.noreply.github.com>
2025-11-15 17:38:35 +0800

    尝试添加手写笔按键支持
    模仿小米平板6代码
    https://github.com/weekdaycare/kernel_xiaomi_sm8250_mod/commit/4aa4b85b1fba61b2ce74db94f8b19bbe2bc7c1ee

--- drivers/input/touchscreen/nt36523/nt36xxx.c
+++ drivers/input/touchscreen/nt36523/nt36xxx.c
@@ -1113,6 +1113,8 @@ static void release_pen_event(void) {
 		input_report_key(ts->pen_input_dev, BTN_TOOL_PEN, 0);
 		input_report_key(ts->pen_input_dev, BTN_STYLUS, 0);
 		input_report_key(ts->pen_input_dev, BTN_STYLUS2, 0);
+		input_report_key(ts->pen_input_dev, KEY_PAGEDOWN, 0);
+		input_report_key(ts->pen_input_dev, KEY_PAGEUP, 0);
 		input_sync(ts->pen_input_dev);
 	}
 }
@@ -1871,6 +1873,8 @@ static irqreturn_t nvt_ts_work_func(int irq, void *data)
 				input_report_key(ts->pen_input_dev, BTN_TOOL_PEN, !!pen_distance || !!pen_pressure);
 				input_report_key(ts->pen_input_dev, BTN_STYLUS, pen_btn1);
 				input_report_key(ts->pen_input_dev, BTN_STYLUS2, pen_btn2);
+				input_report_key(ts->pen_input_dev, KEY_PAGEDOWN, pen_btn1);
+				input_report_key(ts->pen_input_dev, KEY_PAGEUP, pen_btn2);
 				input_sync(ts->pen_input_dev);
 				// TBD: pen battery event report
 				// NVT_LOG("pen_battery=%d\n", pen_battery);
@@ -2938,6 +2942,8 @@ static int32_t nvt_ts_probe(struct spi_device *client)
 		//ts->pen_input_dev->keybit[BIT_WORD(BTN_TOOL_RUBBER)] |= BIT_MASK(BTN_TOOL_RUBBER);
 		ts->pen_input_dev->keybit[BIT_WORD(BTN_STYLUS)] |= BIT_MASK(BTN_STYLUS);
 		ts->pen_input_dev->keybit[BIT_WORD(BTN_STYLUS2)] |= BIT_MASK(BTN_STYLUS2);
+		ts->pen_input_dev->keybit[BIT_WORD(KEY_PAGEDOWN)] |= BIT_MASK(KEY_PAGEDOWN);	
+		ts->pen_input_dev->keybit[BIT_WORD(KEY_PAGEUP)] |= BIT_MASK(KEY_PAGEUP);
 		ts->pen_input_dev->propbit[0] = BIT(INPUT_PROP_DIRECT);
 
 		if (ts->wgp_stylus) {
